a46d5d6262ba055f76b9519e72b830b61c3975f5
mahsa <m.seifikar@samanpr.net>
2020-03-27 02:39:46 +0430

    Specify the days that did not etled!

--- src/data/wikihow_scraper.py
+++ src/data/wikihow_scraper.py
@@ -13,10 +13,8 @@ class WikiHowSpider(scrapy.Spider):
     def parse(self, response):
         request_list = response.xpath('//div[@id="hp_popular"]/'
                                       'div[@id="hp_popular_container"]/'
-                                      'div[@class="hp_thumb"]/'
-                                      'a/'
-                                      '@href').getall()
-
+                                      'div[@class="hp_thumb "]/'
+                                      'a/@href').getall()
         for request in request_list:
             yield response.follow(self.main_url + request,
                                   callback=self.parse_subpage)
@@ -24,8 +22,9 @@ class WikiHowSpider(scrapy.Spider):
     def parse_subpage(self, response):
         title = response.xpath('//title/text()').extract_first()
         path = os.path.join(*[os.getcwd(),
-                              'data/raw',
-                              'trend-%s' %
+                              'data',
+                              'raw',
+                              'trend',
                               datetime.datetime.now().strftime("%Y-%m-%d")])
 
         if not os.path.exists(path):
--- src/etl/wikihow_trend_etl.py
+++ src/etl/wikihow_trend_etl.py
@@ -1,8 +1,14 @@
+import datetime
+import os
 import click
 
 
-def extract(project_name, start_date, end_date):
+def extract(project_name, start_date):
+    """ Extract
+
+    """
     print('extract')
+
     df = ''
     return df
 
@@ -19,8 +25,35 @@ def load(df):
 @click.command()
 @click.option("--project_name", default='trend')
 @click.option("--start_date", default='2020-03-23')
-@click.option("--end_date", default='2020-03024')
+@click.option("--end_date", default='2020-03026')
 def main(project_name, start_date, end_date):
-    df = extract(project_name, start_date, end_date)
-    df = transform(df)
-    load(df)
+    data_path = os.path.join(*['data',
+                               'raw',
+                               project_name])
+    with open('config/conf_%s' % project_name, 'r') as f:
+        file_names = f.read().split()
+    # if file_names is empty run etl from the first available date!
+    if not file_names:
+        start_date = datetime.datetime.strptime(os.listdir(data_path)[0],
+                                                '%Y-%m-%d')
+    else:
+        start_date = datetime.datetime.strptime(file_names[-1],
+                                                '%Y-%m-%d') \
+                     + datetime.timedelta(days=1)
+
+    while os.path.exists(os.path.join(data_path,
+                                      start_date.strftime('%Y-%m-%d'))):
+        print(start_date)
+
+        # df = extract(project_name, start_date)
+        # df = transform(df)
+        # load(df)
+
+        with open('config/conf_%s' % project_name, 'a') as f:
+            f.write(start_date.strftime('%Y-%m-%d'))
+            f.write('\n')
+        start_date += datetime.timedelta(days=1)
+
+
+if __name__ == '__main__':
+    main()
