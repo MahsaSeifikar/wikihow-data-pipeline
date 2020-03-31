7ac12d8a06ee24f0bb2b5fa123b2d55c4ae17652
mahsa <m.seifikar93@gmail.com>
2020-04-01 00:02:34 +0430

    Initialize etl test functions.

---
+++ test/test_wikihow_trend_etl.py
@@ -0,0 +1,16 @@
+# import pytest
+# from src.etl.wikihow_trend_etl import extract
+# from src.etl.wikihow_trend_etl import transform
+# from src.etl.wikihow_trend_etl import load
+
+
+def test_extract():
+    assert False
+
+
+def test_transform():
+    assert False
+
+
+def test_load():
+    assert False
